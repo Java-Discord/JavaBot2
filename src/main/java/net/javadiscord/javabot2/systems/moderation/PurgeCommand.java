1e423229154dab76c4fdabbc65bb704b686a7ce0
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-11-03 13:47:25 +0100

    Renamed command for clarity.

--- src/main/java/net/javadiscord/javabot2/systems/moderation/PurgeCommand.java
+++ src/main/java/net/javadiscord/javabot2/systems/moderation/PurgeCommand.java
@@ -4,9 +4,9 @@ import net.javadiscord.javabot2.command.SlashCommandHandler;
 import org.javacord.api.interaction.SlashCommandInteraction;
 import org.javacord.api.interaction.callback.InteractionImmediateResponseBuilder;
 
-public class PruneCommand implements SlashCommandHandler {
+public class PurgeCommand implements SlashCommandHandler {
 	@Override
-	public InteractionImmediateResponseBuilder handle(SlashCommandInteraction interaction) throws Exception {
+	public InteractionImmediateResponseBuilder handle(SlashCommandInteraction interaction) {
 		return interaction.createImmediateResponder()
 				.append("Not yet implemented");
 	}
